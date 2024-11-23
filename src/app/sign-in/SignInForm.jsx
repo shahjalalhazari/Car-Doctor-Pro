8c680447393d08a390e13cab3d1ebc84e1be1e0d
Shahjalal Hazari <shahjalalhazari1@gmail.com>
2024-11-24 00:08:02 +0400

    Loaing state & toast added on SignIn and SignUp page.

--- src/app/sign-in/SignInForm.jsx
+++ src/app/sign-in/SignInForm.jsx
@@ -1,26 +1,38 @@
 "use client";
 
+import { useState } from "react";
+import { useSearchParams } from "next/navigation";
 import { signIn } from "next-auth/react";
+import { toast } from "react-toastify";
+
 import InputField from "@/components/Authentication/InputField";
 import FullWidthBtn from "@/components/shared/FullWidthBtn";
-import { useRouter, useSearchParams } from "next/navigation";
 
 const SignInForm = () => {
+  const [loading, setLoading] = useState(null);
   const searchParams = useSearchParams();
   const path = searchParams.get("redirect") || "/";
 
   const signInHandler = async (event) => {
+    setLoading(true);
     event.preventDefault();
     const form = event.target;
     const email = form.email.value;
     const password = form.password.value;
 
-    await signIn("credentials", {
+    const res = await signIn("credentials", {
       email,
       password,
       redirect: true,
       callbackUrl: path,
     });
+
+    toast.promise(res, {
+      pending: "You'll be redirected.",
+      success: "Sign In successfully!",
+      error: "Something went wrong.",
+    });
+    setLoading(false);
   };
   return (
     <form onSubmit={signInHandler} className="mt-10 space-y-6">
@@ -38,6 +50,14 @@ const SignInForm = () => {
         type="password"
         placeholder="Your Password"
       />
+
+      {/* loading spinner */}
+      {loading && (
+        <p className="text-secondary text-center">
+          <span className="loading loading-dots loading-xs"></span>
+        </p>
+      )}
+
       {/* Submit Button */}
       <FullWidthBtn text="Sign In" />
     </form>
--- src/app/sign-up/SignUpForm.jsx
+++ src/app/sign-up/SignUpForm.jsx
@@ -3,11 +3,16 @@
 import InputField from "@/components/Authentication/InputField";
 import FullWidthBtn from "@/components/shared/FullWidthBtn";
 import { useState } from "react";
+import { toast } from "react-toastify";
+import { useRouter } from "next/navigation";
 
 const SignUpForm = () => {
+  const router = useRouter();
   const [error, setError] = useState("");
+  const [loading, setLoading] = useState(null);
 
   const SignUpHandler = async (event) => {
+    setLoading(true);
     event.preventDefault();
     const form = event.target;
     const name = form.name.value;
@@ -19,23 +24,33 @@ const SignUpForm = () => {
 
     // Check if password and confirm password match
     if (password !== confirmPassword) {
-      setError("Passwords do not match.");
+      setError("Passwords doesn't match.");
       return;
     }
     const newUser = { name, email, password };
 
     // send POST request to server with user details
-    const resp = await fetch("http://localhost:3000/sign-up/api", {
-      method: "POST",
-      body: JSON.stringify(newUser),
-      headers: {
-        "content-type": "application/json",
-      },
-    });
-    console.log(resp);
+    try {
+      const resp = await fetch("http://localhost:3000/sign-up/api", {
+        method: "POST",
+        body: JSON.stringify(newUser),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
 
-    if (resp.status === 201) {
-      form.reset();
+      if (!resp.ok) {
+        const errorData = await resp.json();
+        setError(errorData.message || "Failed to create account.");
+        return;
+      }
+
+      toast.success("Account created successfully! Please Sign In");
+      setLoading(false);
+      router.push("/sign-in");
+    } catch (error) {
+      console.error("Error:", error);
+      setError("Something went wrong. Please try again later.");
     }
   };
 
@@ -69,6 +84,17 @@ const SignUpForm = () => {
         type="password"
         placeholder="Re-enter Password"
       />
+
+      {/* show error messages */}
+      {error && <p className="text-red-500 text-center">{error}</p>}
+
+      {/* loading spinner */}
+      {loading && (
+        <p className="text-secondary text-center">
+          <span className="loading loading-dots loading-xs"></span>
+        </p>
+      )}
+
       {/* Submit Button */}
       <FullWidthBtn text="Sign Up" />
     </form>
